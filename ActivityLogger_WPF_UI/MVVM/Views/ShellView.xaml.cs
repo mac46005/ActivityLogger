a1158c37de6a56c4e27b6dbf5ccb420fefa9ceb9
Marco Preciado <mac46005@gmail.com>
2022-01-03 22:20:48 -0600

    Added Clock

--- ActivityLogger_WPF_UI/MVVM/Views/ShellView.xaml.cs
+++ ActivityLogger_WPF_UI/MVVM/Views/ShellView.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,9 +20,20 @@ namespace ActivityLogger_WPF_UI.MVVM.Views
     /// </summary>
     public partial class ShellView : Window
     {
+        System.Windows.Threading.DispatcherTimer Timer = new System.Windows.Threading.DispatcherTimer();
         public ShellView()
         {
             InitializeComponent();
+            Timer.Tick += new EventHandler(OnLoaded);
+            Timer.Interval = new TimeSpan(0, 0, 1);
+            Timer.Start();
+        }
+
+        private void OnLoaded(object sender, EventArgs e)
+        {
+            DateTime d;
+            d = DateTime.Now;
+            ClockTextBox.Text = d.Hour + ":" + d.Minute + ":" + d.Second;
         }
     }
 }
