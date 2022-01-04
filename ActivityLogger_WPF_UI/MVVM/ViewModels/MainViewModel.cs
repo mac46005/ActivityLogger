e12ea1874db71033c9e8bbf137c010d4c88a0152
Marco Preciado <mac46005@gmail.com>
2022-01-03 21:29:54 -0600

    Learned some modern UI stuff but i want to custom make my own

---
+++ ActivityLogger_WPF_UI/MVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using ActivityLogger_WPF_UI.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivityLogger_WPF_UI.MVVM.ViewModels
+{
+    class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+
+        }
+    }
+}
---
+++ ActivityLogger_WPF_UI/MVVM/ViewModels/ShellViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivityLogger_WPF_UI.MVVM.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+        IEventAggregator _events;
+        public ShellViewModel(IEventAggregator events)
+        {
+            _events = events;
+            _events.Subscribe(this);
+        }
+    }
+}
