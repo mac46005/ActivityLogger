113cc09da4921b06acbe00dc7de641e568153e34
Marco Preciado <mac46005@gmail.com>
2022-01-24 12:49:29 -0600

    Basic Window functionality: min,max,close

--- ActivityLogger_WPF/MainWindow.xaml.cs
+++ ActivityLogger_WPF/MainWindow.xaml.cs
@@ -24,5 +24,35 @@ namespace ActivityLogger_WPF
         {
             InitializeComponent();
         }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(Application.Current.MainWindow.WindowState == WindowState.Maximized)
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
     }
 }
