189ab3ae0916bdbe66dcaf89f0ac904cbfada4b5
Marco Preciado <mac46005@gmail.com>
2022-01-26 14:21:53 -0600

    Continue on ToDoView Design

---
+++ ActivityLogger_WPF/MVVM/Models/ToDoModel.cs
@@ -0,0 +1,30 @@
+﻿using ActivityLogger_WPF.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivityLogger_WPF.MVVM.Models
+{
+    public class ToDoModel : ObservableObject
+    {
+        private bool _isTaskDone = false;
+
+        public bool IsTaskDone
+        {
+            get { return _isTaskDone; }
+            set { _isTaskDone = value; }
+        }
+
+
+        private string _toDo;
+
+        public string ToDo
+        {
+            get { return _toDo; }
+            set { _toDo = value; }
+        }
+
+    }
+}
---
+++ ActivityLogger_WPF/MVVM/ViewModels/ToDoListViewModel.cs
@@ -0,0 +1,22 @@
+﻿using ActivityLogger_WPF.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivityLogger_WPF.MVVM.ViewModels
+{
+    class ToDoListViewModel : ObservableObject
+    {
+
+        private string _textBoxValue;
+
+        public string TextBoxValue
+        {
+            get { return _textBoxValue; }
+            set { _textBoxValue = value; }
+        }
+
+    }
+}
