8e948108613926580c6bf947f0948b7f7448bb2a
Marco Preciado <mac46005@gmail.com>
2022-01-28 21:19:43 -0600

    Fixing Todolistview control

--- ActivityLogger_WPF/MVVM/ViewModels/ToDoListViewModel.cs
+++ ActivityLogger_WPF/MVVM/ViewModels/ToDoListViewModel.cs
@@ -3,6 +3,7 @@ using ActivityLogger_WPF.MVVM.Models;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -14,24 +15,58 @@ namespace ActivityLogger_WPF.MVVM.ViewModels
 {
     class ToDoListViewModel : ObservableObject
     {
-        public CollectionView ToDoCollectionView;
+        public CollectionViewSource ToDoCollectionViewSource;
+
+
+
+        public ICollectionView ToDoSourceCollection => ToDoCollectionViewSource.View;
+
+
+
+
+
         public ObservableCollection<ToDoModel> ToDoCollection { get; set; } = new ObservableCollection<ToDoModel>();
 
 
 
 
-        private string _textBoxValue;
+
+
+
+
+        private string _textBoxValue = "";
 
         public string TextBoxValue
         {
             get { return _textBoxValue; }
-            set { _textBoxValue = value; }
+            set
+            {
+                _textBoxValue = value;
+                OnPropertyChanged("TextBoxValue");
+                OnPropertyChanged("IsSubmitButtonEnabled");
+            }
         }
+
+        private DateTime _setDueDate = DateTime.Now;
+
+        public DateTime SetDueDate
+        {
+            get { return _setDueDate; }
+            set
+            {
+                _setDueDate = value;
+                OnPropertyChanged("SetDueDate");
+                OnPropertyChanged("IsSubmitButtonEnabled");
+            }
+        }
+
+
+
+
         public ToDoListViewModel()
         {
-            ToDoCollectionView = new CollectionView(ToDoCollection);
 
-            
+
             ClickSubmitCommand = new RelayCommand(o =>
             {
                 ToDoCollection.Add(new ToDoModel { ToDoValue = TextBoxValue });
@@ -41,6 +76,31 @@ namespace ActivityLogger_WPF.MVVM.ViewModels
 
 
 
+
+
+
+
+        private bool _isSubmitButtonEnabled;
+
+        public bool IsSubmitButtonEnabled
+        {
+            get 
+            { 
+                
+                if (TextBoxValue.Length > 0 && SetDueDate > DateTime.Now)
+                {
+                    _isSubmitButtonEnabled = true;
+                }
+                else
+                {
+                    _isSubmitButtonEnabled = false;
+                }
+                return _isSubmitButtonEnabled; 
+            }
+        }
+
+
+        // COMMANDS
         public ICommand ClickSubmitCommand { get; set; }
     }
 }
